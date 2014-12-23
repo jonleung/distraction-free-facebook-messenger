4fdf92e502f63708b740b9b8d3c5388edb38e058
Jonathan Leung <me@jonl.org>
2014-12-23 14:01:04 -0800

    refactor into tabExists fn

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
+var tabExists = function(tabId, callback) {
+	chrome.windows.getAll({ populate: true }, function (windows) {
+		var exists = false;
+		for (var i = 0, window; window = windows[i]; i++) {
+			for (var j = 0, tab; tab = window.tabs[j]; j++) {
+				if (tab.id === tabId) {
+					exists = true
+				}
+			}
+		}
+
+		callback(exists);
+	});
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 
 	chrome.storage.local.get(null, function(storageObject) {
 		var tabId = storageObject.tabId;
 		var windowId = storageObject.windowId;
 
-		chrome.windows.getAll({ populate: true }, function (windows) {
-			var exists = false;
-			for (var i = 0, window; window = windows[i]; i++) {
-				for (var j = 0, tab; tab = window.tabs[j]; j++) {
-					if (tab.id === tabId) {
-						exists = true
-					}
-				}
-			}
+
+		tabExists(tabId, function(exists) {
 			if (exists === true) {
 				chrome.tabs.update(tabId, {selected: true});
 				chrome.windows.update(windowId, {focused: true});
@@ -24,5 +32,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 				});
 			}
 		});
+		
 	});
 });
