ad4e5d787423b17b63847db339af770ef0edef19
Jonathan Leung <me@jonl.org>
2014-12-23 11:39:03 -0800

    open facebook messenger when clicked

--- js/app.js
+++ js/app.js
@@ -3,10 +3,16 @@
 // found in the LICENSE file.
 
 // Called when the user clicks on the browser action.
-// chrome.browserAction.onClicked.addListener(function(tab) {
+
+
+
+chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-//   console.log('Turning ' + tab.url + ' red!');
-//   chrome.tabs.executeScript({
-//     code: 'document.body.style.backgroundColor="red"'
-//   });
-// });
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.create({
+  	url: "https://www.facebook.com/messages/",
+  }, function(tab) {
+  	alet(tab);
+  });
+
+});
---
+++ js/execute.js
@@ -0,0 +1,1 @@
+window.location = "https://www.facebook.com/messages/"
