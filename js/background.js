e5c9fa2a446f535a2065314839e92bbd5f2f9a7d
Jonathan Leung <me@jonl.org>
2014-12-24 01:30:24 -0800

    refactor and comment code

--- js/background.js
+++ js/background.js
@@ -1,4 +1,5 @@
-function executeScripts(tabId, injectDetailsArray) {
+// Run multiple javascript files at the same time
+var executeScripts = function(tabId, injectDetailsArray) {
 	function createCallback(tabId, injectDetails, innerCallback) {
 		return function () {
 			chrome.tabs.executeScript(tabId, injectDetails, innerCallback);
@@ -14,7 +15,7 @@ function executeScripts(tabId, injectDetailsArray) {
     callback();   // execute outermost function
 }
 
-
+// Check to see if a tab exists
 var tabExists = function(tabId, callback) {
 	chrome.windows.getAll({ populate: true }, function (windows) {
 		var exists = false;
@@ -30,25 +31,23 @@ var tabExists = function(tabId, callback) {
 	});
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
 
-	chrome.storage.local.get(null, function(storageObject) {
+chrome.browserAction.onClicked.addListener(function(tab) { // when the extension button is clicked
+
+	chrome.storage.local.get(null, function(storageObject) { // see if the messenger tab from before is still open
 		var tabId = storageObject.tabId;
 		var windowId = storageObject.windowId;
 
-
-		tabExists(tabId, function(exists) {
+		tabExists(tabId, function(exists) { // if it is, focus on that
 			if (exists === true) {
 				chrome.windows.update(windowId, {focused: true});
 				chrome.tabs.update(tabId, {selected: true});
 			}
 			else {
-				chrome.tabs.create({ url: "https://www.facebook.com/messages/" }, function(tab) {
-					chrome.storage.local.set({tabId: tab.id, windowId: tab.windowId});
-
-					chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {//add listener to the url
+				chrome.tabs.create({ url: "https://www.facebook.com/messages/" }, function(tab) { // otherwise open a new messenger tab
+					chrome.storage.local.set({tabId: tab.id, windowId: tab.windowId}); // and remember that you did that
+					chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) { // and then run the scripts on the messenger page
 						if (tabId === tab.id && changeInfo.status === 'complete') {
-							// chrome.tabs.executeScript({ code: 'alert("hello world");' });
 							executeScripts(null, [ 
 							    { file: 'js/lib/jquery-2.1.3.min.js' }, 
 							    { file: "js/strip.js" }
--- js/strip.js
+++ js/strip.js
@@ -1,25 +1,30 @@
 $('#pagelet_bluebar').remove(); // removes the top bar
 $('#rightCol').remove(); // removes the right column
-$('#pagelet_ticker').remove() // removes the annoying mini newsfeed on the top right hand corner
-$('._5qqe').remove(); // removes a tiny box that show up
+$('#pagelet_ticker').remove() // removes the annoying mini-newsfeed on the top right hand corner
+$('._5qqe').remove(); // removes a tiny box is left behind near the mini-newsfeed
 
-// Remove any kind of hover overs
-var style = $('<style>.uiContextualLayerLeft, ._50d1 {display: none;}</style>')
-$('html > head').append(style);
+// Remove profile hover overs
+var style = $('<style>.uiContextualLayerLeft, ._50d1 {display: none;}</style>') // note that the left one is chosen. 
+$('html > head').append(style);																									// If it is the more generic non-sided uiContextualLayer
+																																								// then the dropdowns for various menus would also break
+																																								// instead of making the profile hovers disappear
 
-window.resizeTo(window.outerWidth+1, window.outerHeight+1);
-window.resizeTo(window.outerWidth-1, window.outerHeight-1);
 
-setInterval(function() {
 
+setInterval(function() { // a set timeout is necessary because FB generates new content on the fly in messages
+
+	// neutor all links that hover and bring up information 
+	// when hovered over (usually profiles) so that they are not clickable.
 	$('a[data-hovercard]').each(function(i, aTag) {
 		aTag = $(aTag);
 		aTag.attr('href', '#');
 		aTag.removeAttr('data-hovercard');
 	});	
 
+	// neutor all chat links and profile picture links, respctively so that they are not clickable.
 	$('.titlebarText, a:has(img)').attr('href', '#');
 
+	// Attempted to remove profile links by specifying the regex
 	// $('a').filter(function() {
 	// 	var href = $(this).attr('href');
 	// 	if (href !== undefined) {
@@ -31,8 +36,10 @@ setInterval(function() {
 	// }).attr('href', '#');
 
 
-}, 500);
+}, 500); // this seems to 
 
+
+// Attempted to make links not clickable as they were dynamically inserted into the page.
 // $(document).bind('DOMNodeInserted', function(e) {
 // 	var el = $(e.target);
 
