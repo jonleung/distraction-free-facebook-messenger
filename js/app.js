acff3059890d6d34c02040d9ef199126bc9742fc
Jonathan Leung <me@jonl.org>
2014-12-23 17:18:28 -0800

    removed all distractions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+function executeScripts(tabId, injectDetailsArray) {
+	function createCallback(tabId, injectDetails, innerCallback) {
+		return function () {
+			chrome.tabs.executeScript(tabId, injectDetails, innerCallback);
+		};
+	}
+
+	var callback = null; 
+
+	for (var i = injectDetailsArray.length - 1; i >= 0; --i)
+		callback = createCallback(tabId, injectDetailsArray[i], callback);
+
+	if (callback !== null)
+    callback();   // execute outermost function
+}
+
+
 var tabExists = function(tabId, callback) {
 	chrome.windows.getAll({ populate: true }, function (windows) {
 		var exists = false;
@@ -22,16 +39,25 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 		tabExists(tabId, function(exists) {
 			if (exists === true) {
-				chrome.tabs.update(tabId, {selected: true});
 				chrome.windows.update(windowId, {focused: true});
+				chrome.tabs.update(tabId, {selected: true});
 			}
 			else {
 				chrome.tabs.create({ url: "https://www.facebook.com/messages/" }, function(tab) {
-					chrome.storage.local.set({tabId: tab.id, windowId: tab.windowId}, function(tab) {
+					chrome.storage.local.set({tabId: tab.id, windowId: tab.windowId});
+
+					chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {//add listener to the url
+						if (tabId === tab.id && changeInfo.status === 'complete') {
+							// chrome.tabs.executeScript({ code: 'alert("hello world");' });
+							executeScripts(null, [ 
+							    { file: 'js/lib/jquery-2.1.3.min.js' }, 
+							    { file: "js/modifications.js" }
+							])
+						}	  
 					});
+
 				});
 			}
 		});
-		
 	});
 });
